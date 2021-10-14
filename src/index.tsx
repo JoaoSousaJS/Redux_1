e2c5bbbef5abebb70a7b65a93b62e437ae58a3c0
Joao Sousa <joao.rsousa23@gmail.com>
2021-10-15 00:10:49 +1030

    feat: add provider to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import App from './components/App'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
+import reducers from './reducers'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={createStore(reducers)}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
