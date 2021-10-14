f7c39f2b8f12c17917999568a6b7baf6454a80d4
Joao Sousa <joao.rsousa23@gmail.com>
2021-10-15 00:05:28 +1030

    feat: add song reducer and selectSongReducer

---
+++ src/reducers/index.tsx
@@ -0,0 +1,18 @@
+import { SelectSongs } from '../actions/types/selectSongTypes'
+
+const songsReducer = () => {
+  return [
+    { title: 'No Scrubs', duration: '4:05' },
+    { title: 'Macarena', duration: '2:30' },
+    { title: 'All Star', duration: '3:15' },
+    { title: 'I want it that way', duration: '2:15' }
+  ]
+}
+
+const selectedSongReducer = (selectedSong = null, action: SelectSongs) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload
+  }
+
+  return selectedSong
+}
