248e60ef020aa67f588a81e7db19ba3a02a7696f
Joao Sousa <joao.rsousa23@gmail.com>
2021-10-17 00:58:16 +1030

    feat: render the song list

--- src/components/SongList/index.tsx
+++ src/components/SongList/index.tsx
@@ -2,8 +2,19 @@ import { ConnectedProps } from 'react-redux'
 import { connector } from '../../reducers/songs'
 
 const SongList = (props: ConnectedProps<typeof connector>) => {
-  console.log(props)
-  return <div>SongList</div>
+  const renderList = () => {
+    return props.songs.map((song) => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button className="ui button primary">Select</button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      )
+    })
+  }
+  return <div className="ui divided list">{renderList()}</div>
 }
 
 export default connector(SongList)
