7cb52111ff4e94649de1d9ea3a85b9e61c6b03aa
Joao Sousa <joao.rsousa23@gmail.com>
2021-10-17 01:15:49 +1030

    feat: add selectSong to songs reducer

--- src/reducers/songs/index.tsx
+++ src/reducers/songs/index.tsx
@@ -1,8 +1,11 @@
 import { connect } from 'react-redux'
+import { selectSong } from '../../actions'
 import { ReducerProps } from '../types/reducerType'
 
 const mapStateToProps = (state: ReducerProps) => {
   return { songs: state.songs }
 }
 
-export const connector = connect(mapStateToProps)
+export const connector = connect(mapStateToProps, {
+  selectSong
+})
