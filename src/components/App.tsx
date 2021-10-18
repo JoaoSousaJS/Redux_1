bed9ad0ff39cb7eeb777aca669f6da0656a84e48
Joao Sousa <joao.rsousa23@gmail.com>
2021-10-19 00:16:14 +1030

    feat: render song detail

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,4 @@
+import SongDetails from './SongDetails'
 import SongList from './SongList'
 
 function App() {
@@ -6,6 +7,9 @@ function App() {
       <div className="ui row">
         <div className="column eight wide">
           <SongList />
+        </div>
+        <div className="column eight wide">
+          <SongDetails />
         </div>
       </div>
     </div>
--- src/components/SongDetails/index.tsx
+++ src/components/SongDetails/index.tsx
@@ -1,7 +1,19 @@
+import { ConnectedProps } from 'react-redux'
 import { connector } from '../../reducers/songs'
 
-const SongIndex = () => {
-  return <div>Enter</div>
+const SongIndex = ({ selectedSong }: ConnectedProps<typeof connector>) => {
+  return (
+    <div>
+      <h3>Details for:</h3>
+      {selectedSong && (
+        <p>
+          Title: {selectedSong.title}
+          <br />
+          duration: {selectedSong.duration}
+        </p>
+      )}
+    </div>
+  )
 }
 
 export default connector(SongIndex)
